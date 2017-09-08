37de5efc12a970552dfc439ede59abdd7b0c1454
Yongduek Seo <yndk@sogang.ac.kr>
2017-09-08 16:31:29 +0900

    new

--- samples/gpc_evaluate_sintel.py
+++ samples/gpc_evaluate_sintel.py
@@ -8,20 +8,21 @@ FRAME_DIST = 20
 assert (FRAME_DIST >= 1)
 
 
-def execute(cmd):
-    #print ('execute: ', cmd)
-    popen = subprocess.Popen(cmd,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-    for stdout_line in iter(popen.stdout.readline, ''):
-        print(stdout_line.rstrip())
-    for stderr_line in iter(popen.stderr.readline, ''):
-        print(stderr_line.rstrip())
-    popen.stdout.close()
-    popen.stderr.close()
-    return_code = popen.wait()
-    if return_code != 0:
-        raise subprocess.CalledProcessError(return_code, cmd)
+def execute(cmdlist):
+    for cmd in cmdlist:
+        print ('execute: ', cmd)
+        popen = subprocess.Popen(cmd,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+        for stdout_line in iter(popen.stdout.readline, ''):
+            print(stdout_line.rstrip())
+        for stderr_line in iter(popen.stderr.readline, ''):
+            print(stderr_line.rstrip())
+        popen.stdout.close()
+        popen.stderr.close()
+        return_code = popen.wait()
+        if return_code != 0:
+            raise subprocess.CalledProcessError(return_code, cmd)
 
 
 def main():
@@ -37,10 +38,9 @@ def main():
     parser.add_argument('--gt_path',
                         help='Path to the directory with ground truth flow',
                         required=True)
-    parser.add_argument('--descriptor_type',
-                        help='Descriptor type',
-                        type=int,
-                        default=0)
+    parser.add_argument('--out_path',
+                        help='Path to the directory of output images',
+                        default='./outdir')
     parser.add_argument('--frame_dist',
                         help='Frame Skip Distance', type=int, default=20)
     args = parser.parse_args()
@@ -53,37 +53,27 @@ def main():
     seq.sort()
     input_files = []
 
-    #print ('main() seq=', seq)
+    cmdlist = []
 
     for s in seq:
         seq_name = os.path.basename(s)
         frames = glob.glob(os.path.join(s, 'frame*.png'))
         frames.sort()
 
-        #print ('s=', s)
-        #print ('seq_name=', seq_name)
-        #print ('frames=', frames)
-
         for i in range(0, len(frames) - 1, FRAME_DIST):
             gt_flow = os.path.join(args.gt_path, seq_name,
                                    os.path.basename(frames[i])[0:-4] + '.flo')
             assert (os.path.isfile(gt_flow))
-            input_files += [frames[i], frames[i + 1], gt_flow]
 
+            outimage = os.path.join(args.gt_path, seq_name,
+                                   os.path.basename(frames[i])[0:-4] + '-eval.png')
+            cmd = 'gpc_evaluate --gpu ' + frames[i] + ' ' + frames[i+1] + ' ' + gt_flow + ' ' + outimage
+            cmdlist.append(cmd)
+            print (cmd)
+            #input_files += [frames[i], frames[i + 1], gt_flow]
             #print ('\t\t', [frames[i], frames[i + 1], gt_flow])
 
-    bashcmd = 'time ' \
-              + args.bin_path \
-              + ' --descriptor-type=%d ' % args.descriptor_type \
-              + ' --max-tree-depth=12 --print-progress=0 '
-
-    for ifiles in input_files:
-        bashcmd += '  %s' % ifiles
-
-    print ('@@ Total {} image triplets for training (frame_dist={}).'.format(len(input_files), FRAME_DIST))
-    print ('@@ run in bash: time ', bashcmd)
-
-    #execute([args.bin_path, '--descriptor-type=%d' % args.descriptor_type] + input_files)
+    #execute(cmdlist)
 
 
 if __name__ == '__main__':
